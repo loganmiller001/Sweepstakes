9ae8b0f238852cad9b0ffd35d728815fe30e1741
loganmiller001 <loganmiller001@live.com>
2018-07-11 14:08:50 -0500

    Added SweepstakesManager class to MarketingFirm via Dependency Injection.

---
+++ Sweepstakes/MarketingFirm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    public class MarketingFirm
+    {
+
+        //public readonly ISweepstakesManager _sweepstakesManager;
+
+        //public MarketingFirm() : this(DependencyFactory.Resolve<ISweepstakesManager>())
+        //{
+
+        //}
+
+        //public MarketingFirm(ISweepstakesManager sweepstakesManager)
+        //{
+        //    _sweepstakesManager = sweepstakesManager;
+        //}
+
+        public ISweepstakesManager SweepstakesManager { get; set; }
+
+    }
+        public class SweepstakesManager
+        {
+
+        }
+ }
