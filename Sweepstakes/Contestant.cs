9c866cf5f9ea880fe4b2381c1b5890fcc1c76378
loganmiller001 <loganmiller001@live.com>
2018-07-11 09:27:36 -0500

    Created contestant class, constuctor made to be able to set full name, email, and registration number whenever a new instance of the class is created.

---
+++ Sweepstakes/Contestant.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    public class Contestant
+    {
+        int registrationNumber;
+        string firstName;
+        string lastName;
+        string emailAddress;
+
+
+        public Contestant(string firstName, string lastName, string emailAddress, int registrationNumber)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.emailAddress = emailAddress;
+            this.registrationNumber = registrationNumber;
+        }
+
+        public string Email
+        {
+            get => emailAddress;
+            set => emailAddress = value;
+
+        }
+        public string contestantFirstName
+        {
+            get => firstName;
+            set => firstName = value;
+        }
+
+        public string contestantLastName
+        {
+            get => lastName;
+            set => lastName = value;
+        }
+
+        public int contestantRegistrationNumber
+        {
+            get => registrationNumber;
+            set => registrationNumber = value;
+        }
+    }
+}
