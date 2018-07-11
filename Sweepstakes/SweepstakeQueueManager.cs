669f43115fddd5a5685932c6c942310d2779582a
loganmiller001 <loganmiller001@live.com>
2018-07-11 11:57:26 -0500

    Created queue and stack classes, and made them implement Sweepstakes manager interface.

---
+++ Sweepstakes/SweepstakeQueueManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    public class SweepstakeQueueManager:ISweepstakesManager
+    {
+
+        public SweepstakeQueueManager()
+        {
+
+        }
+        public static void Queue()
+        {
+            Queue<string> sweepstakesQueue = new Queue<string>();
+            sweepstakesQueue.Enqueue("Sweepstake1");
+            sweepstakesQueue.Enqueue("Sweepstake2");
+            sweepstakesQueue.Enqueue("Sweepstake3");
+            sweepstakesQueue.Enqueue("Sweepstake4");
+        }
+
+        public Sweepstakes GetSweepstakes()
+        {
+            Sweepstakes sweepstakes = new Sweepstakes("");
+            return sweepstakes;
+        }
+
+        public void InsertSweepstakes(Sweepstakes sweepstakes)
+        {
+            GetSweepstakes();
+        }
+
+    }
+}
---
+++ Sweepstakes/SweepstakeStackManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    public class SweepstakeStackManager:ISweepstakesManager
+    {
+
+        public SweepstakeStackManager()
+        {
+
+        }
+        public static void Stack()
+        {
+            Stack<string> sweepstakeStack = new Stack<string>();
+            sweepstakeStack.Push("SweepStake1");
+            sweepstakeStack.Push("SweepStake2");
+            sweepstakeStack.Push("SweepStake3");
+            sweepstakeStack.Push("SweepStake4");
+
+        }
+
+        public Sweepstakes GetSweepstakes()
+        {
+            Sweepstakes sweepstakes = new Sweepstakes("");
+            return sweepstakes;
+        }
+
+        public void InsertSweepstakes(Sweepstakes sweepstakes)
+        {
+            GetSweepstakes();
+        }
+    }
+}
